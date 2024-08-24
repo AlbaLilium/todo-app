6a1cdce43cc074511861a12cbc07c2751a6f5a48
Lilija Karpenka <flos.alba.lilium@gmail.com>
2024-08-25 02:03:07 +0300

    fix: add routes to startapp

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
 
+from app.api.v1.tasks_router import tasks_router
+from app.api.v1.users_router import user_router
+from app.api.v1.auth_router import auth_router
 
 app = FastAPI()
+app.include_router(tasks_router)
+app.include_router(user_router)
+app.include_router(auth_router)
