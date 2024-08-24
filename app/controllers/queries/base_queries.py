a551940ec7fd3ce1aa69e8c6251934aaa7fc0614
Lilija Karpenka <flos.alba.lilium@gmail.com>
2024-08-24 17:49:57 +0300

    fix(controller): return object for session control

--- app/controllers/queries/base_queries.py
+++ app/controllers/queries/base_queries.py
@@ -6,6 +6,7 @@ from app.db.db_connection import SessionLocal
 class BaseOperation:
     def __enter__(self):
         self.session = SessionLocal()
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         try:
