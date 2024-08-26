143c3c69c2fd5108fee797e91b3310198b510f64
Lilija Karpenka <flos.alba.lilium@gmail.com>
2024-08-26 10:08:11 +0300

    fix!: move database to asyncpg driver

--- app/db/db_connection.py
+++ app/db/db_connection.py
@@ -1,17 +1,16 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 # from .config import settings
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@postgres:5432/postgres"
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+asyncpg://postgres:postgres@postgres:5432/postgres"
+)
 # settings.database_url_psycopg2
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    echo=True
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+AsyncSession = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()
+metadata = Base.metadata
