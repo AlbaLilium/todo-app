2d7286f204109922c7925726c1bdabca63d0a3c3
Lilia <89929726+AlbaLilium@users.noreply.github.com>
2024-08-27 13:32:31 +0300

    fix: update imports and columns type

--- app/data/models/task_model.py
+++ app/data/models/task_model.py
@@ -1,12 +1,13 @@
 from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+
 from app.db.db_connection import Base
 
 
 class Task(Base):
     __tablename__ = "task"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(15))
+    title: Mapped[str] = mapped_column(String(50))
     description: Mapped[str | None] = mapped_column(String(200), nullable=True)
-    status = mapped_column(String(20), nullable=False) #Enum(*[e.value for e in TaskStatusEnum])
+    status = mapped_column(String(20), nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
