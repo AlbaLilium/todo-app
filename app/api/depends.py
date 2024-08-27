f5dac3bea064783d154e86ccb7704ceb91fca59e
Lilia <89929726+AlbaLilium@users.noreply.github.com>
2024-08-27 13:23:55 +0300

    refactor: delete unnesesary function

--- app/api/depends.py
+++ app/api/depends.py
@@ -1,13 +1,8 @@
-from typing import Annotated
-
 from passlib.exc import UnknownHashError
-from fastapi import Depends, HTTPException, status
 
 from app.controllers.queries.user_queries import UserOperation
-from app.controllers.token import verify_access_token, verify_password
-from app.controllers.utils import oauth2_scheme
-from app.data.serealizers.user_serializer import (UserBase,
-                                                  UserGetRequestSerializer, UserAuthRequestSerializer)
+from app.controllers.token import  verify_password
+from app.data.serealizers.user_serializer import (UserAuthRequestSerializer)
 
 
 async def has_authenticated_user(username: str, password: str) -> bool:
@@ -24,19 +19,3 @@ async def has_authenticated_user(username: str, password: str) -> bool:
         finally:
             return True
 
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> UserBase:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    token_data = verify_access_token(token, credentials_exception)
-
-    async with UserOperation() as db:
-        try:
-            user = await db.get_user(UserGetRequestSerializer(username=token_data.username))
-        except HTTPException(status_code=400, detail="User not found"):
-            raise credentials_exception
-
-    return user
