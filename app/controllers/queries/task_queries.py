f42d857f3d56a8f605d366d1c8e27376006adce5
Lilia <89929726+AlbaLilium@users.noreply.github.com>
2024-08-27 13:30:55 +0300

    fix: rewrite opearation for support async work

--- app/controllers/queries/task_queries.py
+++ app/controllers/queries/task_queries.py
@@ -1,9 +1,10 @@
 from fastapi import HTTPException
-from sqlalchemy import select, delete
+from sqlalchemy import delete, select
 
 from app.controllers.queries.base_queries import BaseOperation
 from app.data.models.task_model import Task as TaskModel
-from app.data.serealizers.task_serializer import (SingleTaskRequestSerializer,
+from app.data.serealizers.task_serializer import (CreateTaskSerializer,
+                                                  SingleTaskRequestSerializer,
                                                   TaskBase,
                                                   TaskListResponseSerializer,
                                                   TaskStatusRequestSerializer,
@@ -12,10 +13,9 @@ from app.data.serealizers.user_serializer import UserGetRequestSerializer
 
 
 class TaskOperation(BaseOperation):
-    async def get_task(self, task: SingleTaskRequestSerializer) -> TaskListResponseSerializer:
-
+    async def get_task(self, task_id: int) -> TaskListResponseSerializer:
         query = await self.session.execute(
-            select(TaskModel).where(TaskModel.id == task.task_id)
+            select(TaskModel).where(TaskModel.id == task_id)
         )
         query_obj_result = query.scalars().all()
         if not query_obj_result:
@@ -24,95 +24,86 @@ class TaskOperation(BaseOperation):
         return TaskListResponseSerializer(tasks=query_obj_result)
 
     async def get_all_tasks(
-            self, page_size: int, page_number: int
+        self, page_size: int, page_number: int
     ) -> TaskListResponseSerializer:
-
-        query_obj_result = select(TaskModel)
-        query_obj_result = self.paginate(query_obj_result, page_size, page_number)
-        query_obj_result = await self.session.execute(query_obj_result)
-        return TaskListResponseSerializer(tasks=query_obj_result.scalars().all())
-
-    async def update_task_by_field(self, task: TaskUpdateRequestSerializer) -> TaskBase:
-
-        task_obj = await self.session.execute(select(TaskModel).where(TaskModel.id == task.task_id))
-        task_obj = task_obj.scalars().one()
-        if not task_obj:
-            raise HTTPException(status_code=400, detail="Object does not exist")
-
-        if task.title:
-            task_obj.title = task.title
-        elif task.status:
-            task_obj.status = task.status
-        elif task.description:
-            task_obj.description = task.description
-
-        self.session.add(task_obj)
-
-        return TaskBase(
-            title=task_obj.title,
-            description=task_obj.description,
-            status=task_obj.status,
-            user_id=task_obj.user_id,
-        )
-
-    async def insert_task(self, task: TaskBase) -> TaskBase:
-
-        task_obj = TaskModel(
-            title=task.title,
-            status=task.status,
-            user_id=task.user_id,
-            description=task.description,
-        )
-        self.session.add(task_obj)
-
-        return TaskBase(
-            title=task_obj.title,
-            description=task_obj.description,
-            status=task_obj.status,
-            user_id=task_obj.user_id,
-        )
-
-    async def delete_task(self, task: SingleTaskRequestSerializer) -> str:
-
-        task_obj = await self.session.execute(
-            delete(TaskModel).where(TaskModel.id == task.task_id)
-        )
-        task_obj = task_obj.scalars().all()
-        if not task_obj:
-            raise HTTPException(status_code=400, detail="Object does not exist")
-
-        return "Object is deleted successfully"
-
-    async def filter_by_status(
-            self, task: TaskStatusRequestSerializer, page_size, page_number
-    ) -> TaskListResponseSerializer:
-
-        if task.user_id:
-            query_obj_result = select(TaskModel).where(
-                TaskModel.user_id == task.user_id,
-                TaskModel.status == task.status,
-            )
-
-        else:
-            query_obj_result = select(TaskModel).where(
-                TaskModel.status == task.status
-            )
-        query_obj_result = self.paginate(
-            query_obj_result, page_size=page_size, page_number=page_number
-        )
-
+        query_obj_result = self.paginate(select(TaskModel), page_size, page_number)
         query_obj_result = await self.session.execute(query_obj_result)
         query_obj_result = query_obj_result.scalars().all()
         return TaskListResponseSerializer(tasks=query_obj_result)
 
-    async def get_users_tasks(
-            self, user: UserGetRequestSerializer, page_size: int, page_number: int
-    ):
-        query_obj_result = select(TaskModel).where(TaskModel.user_id == user.id)
-        query_obj_result = self.paginate(query_obj_result, page_size, page_number)
-        query_obj_result = await self.session.execute(query_obj_result)
-        query_obj_result = query_obj_result.scalars().all()
-        if not query_obj_result:
-            raise HTTPException(status_code=400, detail="Object does not exist")
 
-        return TaskListResponseSerializer(tasks=query_obj_result)
+async def update_task_by_field(self, task: TaskUpdateRequestSerializer) -> TaskBase:
+    task_obj = await self.session.execute(
+        select(TaskModel).where(TaskModel.id == task.id)
+    )
+    task_obj = task_obj.scalars().one()
+    if not task_obj:
+        raise HTTPException(status_code=400, detail="Object does not exist")
+
+    if task.title:
+        task_obj.title = task.title
+    if task.status:
+        task_obj.status = task.status
+    if task.description:
+        task_obj.description = task.description
+
+    self.session.add(task_obj)
+    self.session.commit()
+    self.session.refresh(task_obj)
+
+    return TaskBase(
+        id=task.id,
+        title=task_obj.title,
+        description=task_obj.description,
+        status=task_obj.status,
+        user_id=task_obj.user_id,
+    )
+
+
+async def insert_task(self, task: CreateTaskSerializer) -> int:
+    task_obj = TaskModel(
+        title=task.title,
+        status=task.status,
+        user_id=task.user_id,
+        description=task.description,
+    )
+    self.session.add(task_obj)
+    await self.session.commit()
+    await self.session.refresh(task_obj)
+    return task_obj.id
+
+
+async def delete_task(self, task: SingleTaskRequestSerializer) -> dict[str:str]:
+    await self.session.execute(delete(TaskModel).where(TaskModel.id == task.id))
+    return {"success": "task removed"}
+
+
+async def filter_by_status(
+    self, task: TaskStatusRequestSerializer, page_size, page_number
+) -> TaskListResponseSerializer:
+    if task.user_id:
+        query_obj_result = select(TaskModel).where(
+            TaskModel.user_id == task.user_id,
+            TaskModel.status == task.status,
+        )
+
+    else:
+        query_obj_result = select(TaskModel).where(TaskModel.status == task.status)
+    query_obj_result = self.paginate(
+        query_obj_result, page_size=page_size, page_number=page_number
+    )
+
+    query_obj_result = await self.session.execute(query_obj_result)
+    query_obj_result = query_obj_result.scalars().all()
+    return TaskListResponseSerializer(tasks=query_obj_result)
+
+
+async def get_users_tasks(self, user_id: int, page_size: int, page_number: int):
+    query_obj_result = select(TaskModel).where(TaskModel.user_id == user_id)
+    query_obj_result = self.paginate(query_obj_result, page_size, page_number)
+    query_obj_result = await self.session.execute(query_obj_result)
+    query_obj_result = query_obj_result.scalars().all()
+    if not query_obj_result:
+        raise HTTPException(status_code=400, detail="Object does not exist")
+
+    return TaskListResponseSerializer(tasks=query_obj_result)
--- app/controllers/queries/user_queries.py
+++ app/controllers/queries/user_queries.py
@@ -1,16 +1,18 @@
 from fastapi import HTTPException
-from sqlalchemy import select, or_
+from sqlalchemy import or_, select
 
 from app.controllers.queries.base_queries import BaseOperation
 from app.data.models.user_model import User as UserModel
-from app.data.serealizers.user_serializer import (UserBase,
+from app.data.serealizers.user_serializer import (UserAuthRequestSerializer,
+                                                  UserBase,
                                                   UserCheckRequestSerializer,
                                                   UserCreateRequestSerializer,
-                                                  UserGetRequestSerializer, UserAuthRequestSerializer)
+                                                  UserGetRequestSerializer,
+                                                  UsersListResponseSerializer)
 
 
 class UserOperation(BaseOperation):
-    async def insert_user(self, user: UserCreateRequestSerializer) -> UserBase:
+    async def insert_user(self, user: UserCreateRequestSerializer) -> int:
 
         if await self.check_user(UserCheckRequestSerializer(username=user.username)):
             raise HTTPException(status_code=400, detail="User is already excited")
@@ -22,20 +24,18 @@ class UserOperation(BaseOperation):
             last_name=user.last_name,
         )
         self.session.add(user_obj)
+        self.session.commit()
+        self.session.refresh(user_obj)
 
-        return UserBase(
-            id=user_obj.id,
-            first_name=user_obj.first_name,
-            username=user_obj.username,
-            last_name=user_obj.last_name,
+        return user_obj.id
+
+    async def get_user_by_id(
+        self, user: UserGetRequestSerializer | UserCheckRequestSerializer
+    ) -> UserBase:
+        user = await self.session.execute(
+            select(UserModel).where(UserModel.id == user.id)
         )
 
-    async def get_user(self, user: UserGetRequestSerializer | UserCheckRequestSerializer) -> UserBase:
-        user = await self.session.execute(
-            select(UserModel).where(
-                or_(UserModel.id == user.id,
-                    UserModel.username == user.username))
-            )
         user = user.scalars().first()
 
         if not user:
@@ -48,8 +48,33 @@ class UserOperation(BaseOperation):
             password=user.password,
         )
 
-    async def check_user(self, user: UserCheckRequestSerializer | UserAuthRequestSerializer) -> bool | int:
+    async def get_user_by_username(self, user: UserCheckRequestSerializer) -> UserBase:
         user = await self.session.execute(
-            select(UserModel).where(UserModel.username == user.username))
+            select(UserModel).where(UserModel.username == user.username)
+        )
+
+        user = user.scalars().first()
+
+        if not user:
+            raise HTTPException(status_code=400, detail="User not found")
+        return UserBase(
+            id=user.id,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            username=user.username,
+            password=user.password,
+        )
+
+    async def check_user(
+        self, user: UserCheckRequestSerializer | UserAuthRequestSerializer
+    ) -> bool | int:
+        user = await self.session.execute(
+            select(UserModel).where(UserModel.username == user.username)
+        )
         user = user.scalars().first()
         return True if user else False
+
+    # async def get_users(self):
+    #     users = await self.session.execute(select(UserModel))
+    #     users = users.scalars().all()
+    #     return UsersListResponseSerializer(users_list=users)
